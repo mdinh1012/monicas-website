d5ed7ac124450ed1815e6de460f9c36187b1063b
Monica Dinh <mdinh3@uw.edu>
2024-04-08 18:01:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Initial array of interests
+  const [interests, setInterests] = useState([
+    'Traveling',
+    'Cooking',
+    'Working out',
+    'Going to Music festivals',
+    'Watching thrillers',
+  ]);
+
+  // Function to reorder the list of interests
+  const reorderInterests = () => {
+    const newOrder = [...interests];
+    newOrder.push(newOrder.shift()); // Move the first item to the end
+    setInterests(newOrder); // Update the state to re-render the list
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Welcome!</h1>
+        <p>Hello, I am Monica. </p>
+         <p> Welcome to my personal website where I share a little bit about myself. </p>
       </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>I'm Monica, a passionate explorer of the world, from cuisine to culture. Here's a glimpse into my life and what I love to do.
+            I am currently a 4th year student at The University of Washington in the CSSE program.
+          </p>
+          <img src="/monica.jpg" alt="Monica Dinh" width={250} height={300} />
+        </section>
+        <section>
+          <h2>Things I enjoy:</h2>
+          <ul>
+            {interests.map((interest, index) => (
+              <li key={index}>{interest}</li>
+            ))}
+          </ul>
+          <button onClick={reorderInterests}>Reorder Interests</button>
+        </section>
+      </main>
     </div>
   );
 }
